668b0bf688374d13da8022efeaf81aa2944af16f
Dianaaraujosilva <102118579+Dianaaraujosilva@users.noreply.github.com>
2022-11-15 21:06:00 -0300

    Update ComputerController.cs

--- Controllers/ComputerController.cs
+++ Controllers/ComputerController.cs
@@ -6,12 +6,15 @@ namespace MvcLabManager.Controllers;
 public class ComputerController : Controller
 {
     private LabManagerContext _context;
+
     public ComputerController(LabManagerContext context)
     {
         _context = context;
     }
+
     public IActionResult Index()
     {
+
         return View(_context.Computers);
     }
 
@@ -21,9 +24,55 @@ public class ComputerController : Controller
 
         if(computer == null)
         {
+            return NotFound(); //return RedirectToAction("Index");
+        }
+
+        return View(computer);
+    }
+
+     public IActionResult Cadastro()
+    {
+        return View();
+    }
+
+    public IActionResult ValidacaoCadastro([FromForm] int id, [FromForm] string ram, [FromForm] string processor)
+    {
+        if(_context.Computers.Find(id) != null)
+        {
+            return View(id);
+        }
+        else
+        {
+            _context.Computers.Add(new Computer(id, ram + "GB", processor));
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    public IActionResult Atualizacao(int id)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
             return NotFound();
         }
 
         return View(computer);
+    }
+
+    public IActionResult Update([FromForm] int id, [FromForm] string ram, [FromForm] string processor)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer != null)
+        {
+            computer.Ram = ram + "GB";
+            computer.Processor = processor;
+            _context.Computers.Update(computer);
+            _context.SaveChanges();
+        }
+        return RedirectToAction("Index");
     }
 }
